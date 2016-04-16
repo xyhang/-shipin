24c17ad5077272e7dc23b7aa460ed3a6752bac66
xyhang <929472555@qq.com>
2016-04-16 20:08:27 +0800

    Initial Commit

---
+++ 仿shipin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  仿shipin
+//
+//  Created by yan on 16/4/16.
+//  Copyright © 2016年 yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 仿shipin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  仿shipin
+//
+//  Created by yan on 16/4/16.
+//  Copyright © 2016年 yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
